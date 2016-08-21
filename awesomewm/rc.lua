eec1bf7ba971992e028ce19b24a08de20065c08f
Alexis Couronne <alexis.couronne@gandi.net>
2016-08-21 08:56:54 +0200

    Update tag names in awesomewm config file

--- awesomewm/rc.lua
+++ awesomewm/rc.lua
@@ -80,10 +80,18 @@ end
 
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
-tags = {}
+tags = {
+  names = { 
+    "★ Terminal",
+    "❉ Web",
+    "☠ Code",
+    "☯ Messaging",
+    "♫ Music"
+  }
+}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag(tags.names, s, layouts[1])
 end
 -- }}}
 
