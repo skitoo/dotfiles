8333533d2e478265045d4fbd84144686170089cb
Alexis Couronne <alexis.couronne@gandi.net>
2016-08-24 09:09:28 +0200

    Add default wallpaper management in awesome theme

--- awesomewm/themes/skitoo/theme.lua
+++ awesomewm/themes/skitoo/theme.lua
@@ -77,7 +77,12 @@ theme.titlebar_maximized_button_focus_inactive  = current_directory .. "/titleba
 theme.titlebar_maximized_button_normal_active = current_directory .. "/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = current_directory .. "/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = current_directory .. "/background.png"
+default_wallpaper = home_directory .. "/Pictures/wallpaper.jpg"
+if io.open(default_wallpaper, "r") == nil then
+    theme.wallpaper = current_directory .. "/background.png"
+else
+    theme.wallpaper = default_wallpaper
+end
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = current_directory .. "/layouts/fairhw.png"
